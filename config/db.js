fa5e78aec214eb22a307ed8648971aa798ec43e0
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2025-04-27 22:04:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
-const mongoose  = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, useUnifiedTopology: true 
-        })
-        console.log("connected to DB")
-    } catch (error) {
-        console.log(error);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+  }
+};
 
 module.exports = connectDB;
